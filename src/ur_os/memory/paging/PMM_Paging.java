3e5cf41e99b3414acb065e499a8929274ce2386f
QueenR1014 <juanreinareyes@hotmail.com>
2025-10-08 13:54:33 -0500

    Paging

--- src/ur_os/memory/paging/PMM_Paging.java
+++ src/ur_os/memory/paging/PMM_Paging.java
@@ -1,174 +1,201 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ur_os.memory.paging;
+    /*
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+    */
+    package ur_os.memory.paging;
 
-import ur_os.memory.MemoryAddress;
-import ur_os.memory.ProcessMemoryManager;
-import ur_os.memory.MemoryManagerType;
-import ur_os.system.OS;
-import ur_os.virtualmemory.ProcessVirtualMemoryManager;
+    import javax.swing.plaf.metal.MetalRadioButtonUI;
 
-/**
- *
- * @author super
- */
-public class PMM_Paging extends ProcessMemoryManager{
-    
-    PageTable pt;
-    PageTable vpt;
-    int assignedPages;
-    int loadedPages;
+    import ur_os.memory.MemoryAddress;
+    import ur_os.memory.ProcessMemoryManager;
+    import ur_os.memory.MemoryManagerType;
+    import ur_os.system.OS;
+    import ur_os.virtualmemory.ProcessVirtualMemoryManager;
 
-    public PMM_Paging(int processSize, int assignedPages) {
-        this(null,processSize,assignedPages);
-    }
-    
-    public PMM_Paging(ur_os.process.Process p, int processSize, int assignedPages){
-        super(p, MemoryManagerType.PAGING,processSize);
-        pt = new PageTable(processSize,assignedPages,true);
-        vpt = new PageTable(processSize,assignedPages,true);
-        this.assignedPages = assignedPages; //Number of frames assigned to the process
-        this.loadedPages = 0; //Number of loaded pages of the process
-    }
-    
-    public PMM_Paging(int processSize) {
-        this(processSize,3);
-    }
+    /**
+     *
+     * @author super
+     */
+    public class PMM_Paging extends ProcessMemoryManager{
+        
+        PageTable pt;
+        PageTable vpt;
+        int assignedPages;
+        int loadedPages;
 
-    public PMM_Paging(PMM_Paging pmm) {
-        super(pmm);
-        if(pmm.getType() == this.getType()){
-            this.pt = new PageTable(pmm.getPT());
-            this.vpt = new PageTable(pmm.getVPT());
-            this.assignedPages = pmm.assignedPages;
-            this.loadedPages = pmm.loadedPages;
-        }else{
-            System.out.println("Error - Wrong PMM parameter");
+        public PMM_Paging(int processSize, int assignedPages) {
+            this(null,processSize,assignedPages);
         }
-    }
+        
+        public PMM_Paging(ur_os.process.Process p, int processSize, int assignedPages){
+            super(p, MemoryManagerType.PAGING,processSize);
+            pt = new PageTable(processSize,assignedPages,true);
+            vpt = new PageTable(processSize,assignedPages,true);
+            this.assignedPages = assignedPages; //Number of frames assigned to the process
+            this.loadedPages = 0; //Number of loaded pages of the process
+        }
+        
+        public PMM_Paging(int processSize) {
+            this(processSize,3);
+        }
 
-    public int getAssignedPages() {
-        return assignedPages;
-    }
+        public PMM_Paging(PMM_Paging pmm) {
+            super(pmm);
+            if(pmm.getType() == this.getType()){
+                this.pt = new PageTable(pmm.getPT());
+                this.vpt = new PageTable(pmm.getVPT());
+                this.assignedPages = pmm.assignedPages;
+                this.loadedPages = pmm.loadedPages;
+            }else{
+                System.out.println("Error - Wrong PMM parameter");
+            }
+        }
 
-    public void setAssignedPages(int assignedPages) {
-        if(assignedPages > 0)
-            this.assignedPages = assignedPages;
-        else
-            this.assignedPages = vpt.size;
-    }
+        public int getAssignedPages() {
+            return assignedPages;
+        }
 
-    public int getLoadedPages() {
-        return loadedPages;
-    }
+        public void setAssignedPages(int assignedPages) {
+            if(assignedPages > 0)
+                this.assignedPages = assignedPages;
+            else
+                this.assignedPages = vpt.size;
+        }
 
-    public void setLoadedPages(int loadedPages) {
-        this.loadedPages = loadedPages;
-    }
+        public int getLoadedPages() {
+            return loadedPages;
+        }
 
-    
-    
-    public PageTable getVPT() {
-        return vpt;
-    }
-    
-    public PageTable getPT() {
-        return pt;
-    }
-    
-    public void addFrameID(int frame){
-        pt.addFrameID(frame);
-    }
-    
-    public void addFrameID(int frame, boolean valid){
-        pt.addFrameID(frame, valid);
-    }
-    
-    public void addVFrameID(int frame){
-        vpt.addFrameID(frame);
-    }
-    
-    public void addVFrameID(int frame, boolean valid){
-        vpt.addFrameID(frame,valid);
-    }
-    
-    public void setFrameID(int page, int frame){
-        pt.setFrameID(page, frame);
-        setPageValid(page, true);
-        pt.setPageDirty(page, false);
-    }
-    
-    public MemoryAddress getPageMemoryAddressFromLocalAddress(int locAdd){
-        
-        //Include your code here
-        
-        return new MemoryAddress(-1, -1);
-    }
-    
-    public int getFrameMemoryAddressFromLogicalMemoryAddress(int page){
-        
-        //Include your code here
-        
-        return -1;
-    }
-    
-    public MemoryAddress getFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m){
-        
-        //Include your code here
-        
-        return new MemoryAddress(-1, -1);
-    }
-    
-    
-    public int getVFrameMemoryAddressFromLogicalMemoryAddress(int page){
-        return getVFrameMemoryAddressFromLogicalMemoryAddress(new MemoryAddress(page, 0)).getDivision();
-    }
-    
-    public MemoryAddress getVFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m){
-        
-        //Include your code here
-        
-        return new MemoryAddress(-1, -1);
-    }
-    
-   
-    
-     @Override
-    public String toString(){
-        return pt.toString();
-    }
+        public void setLoadedPages(int loadedPages) {
+            this.loadedPages = loadedPages;
+        }
 
-    public int getFrameInSwap(int page) {
-        return vpt.getFrameIdFromPage(page);
-    }
-    
-    public void setPageValid(int page, boolean valid){
-        pt.setPageValid(page, valid);
-        if(!valid)
-            this.loadedPages--;
-        else
-            this.loadedPages++;
-    }
-    
-    public boolean isPageDirty(int page){
-        return pt.isPageDirty(page);
-    }
-    
-    public void setPageDirty(int page, boolean valid){
-        pt.setPageDirty(page, valid);
-    }
-    
-    
-    
+        
+        
+        public PageTable getVPT() {
+            return vpt;
+        }
+        
+        public PageTable getPT() {
+            return pt;
+        }
+        
+        public void addFrameID(int frame){
+            pt.addFrameID(frame);
+        }
+        
+        public void addFrameID(int frame, boolean valid){
+            pt.addFrameID(frame, valid);
+        }
+        
+        public void addVFrameID(int frame){
+            vpt.addFrameID(frame);
+        }
+        
+        public void addVFrameID(int frame, boolean valid){
+            vpt.addFrameID(frame,valid);
+        }
+        
+        public void setFrameID(int page, int frame){
+            pt.setFrameID(page, frame);
+            setPageValid(page, true);
+            pt.setPageDirty(page, false);
+        }
+        
+        public MemoryAddress getPageMemoryAddressFromLocalAddress(int locAdd){
+            if(locAdd < 0) System.out.println("Error: Negative valued Local Address");
+            
 
-    @Override
-    public int getVictim(){
-        if(this.loadedPages == this.assignedPages)
-            return pvmm.getVictim(memoryAccesses,this.loadedPages);
-        else
-            return -1;
-    }
+            final int page = locAdd / OS.PAGE_SIZE;
+            final int offset = locAdd % OS.PAGE_SIZE;
+            
+            return new MemoryAddress(page,offset);
+        }
+        
+        public int getFrameMemoryAddressFromLogicalMemoryAddress(int page){
+            
+            if(page < 0) System.out.println("Error: Negative page value");
+            //Check page validity
+            if(!pt.isPageValid(page)) return -1;
+
+            int frame = pt.getFrameIdFromPage(page);
+            return frame * OS.PAGE_SIZE; //return address
+        }
+        
+        public MemoryAddress getFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m){
+            int page = m.getDivision();
+            int offset = m.getOffset();
+
+            //Check page validity
+            if(!pt.isPageValid(page)) return new MemoryAddress(-1,-1);
+
+            int frame = pt.getFrameIdFromPage(page);
+            return new MemoryAddress(frame, offset);
+        }
+        
+        
+        public int getVFrameMemoryAddressFromLogicalMemoryAddress(int page){
+            return getVFrameMemoryAddressFromLogicalMemoryAddress(new MemoryAddress(page, 0)).getDivision();
+        }
+        
+        public MemoryAddress getVFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m){
+            
+            if(m == null) return new MemoryAddress(-1,-1);
+            int page = m.getDivision() / OS.PAGE_SIZE;
+            int offset = m.getOffset();
+            
+            if(page < 0){
+                System.out.println("Error: Negative page number.");
+                return new MemoryAddress(-1,-1);
+            }
+
+            //Check validity and get vpt frame number
+            int vframe = (vpt != null) ? vpt.getFrameIdFromPage(page) : -1;
+            if(vframe < 0) {
+                System.out.println("Error: Logical Address not in swap area");   
+                return new MemoryAddress(-1,-1); 
+            }
+            
+            int vdivision = vframe * OS.PAGE_SIZE;
+            return new MemoryAddress(vdivision, offset);
+        }
+        
     
-}
+        
+        @Override
+        public String toString(){
+            return pt.toString();
+        }
+
+        public int getFrameInSwap(int page) {
+            return vpt.getFrameIdFromPage(page);
+        }
+        
+        public void setPageValid(int page, boolean valid){
+            pt.setPageValid(page, valid);
+            if(!valid)
+                this.loadedPages--;
+            else
+                this.loadedPages++;
+        }
+        
+        public boolean isPageDirty(int page){
+            return pt.isPageDirty(page);
+        }
+        
+        public void setPageDirty(int page, boolean valid){
+            pt.setPageDirty(page, valid);
+        }
+        
+        
+        
+
+        @Override
+        public int getVictim(){
+            if(this.loadedPages == this.assignedPages)
+                return pvmm.getVictim(memoryAccesses,this.loadedPages);
+            else
+                return -1;
+        }
+        
+    }
