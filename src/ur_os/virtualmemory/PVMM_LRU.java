99adeac7969b0bf3c1446ac20cb65d8806b7ca68
QueenR1014 <juanreinareyes@hotmail.com>
2025-11-09 16:31:13 -0500

    Update PVMM_LRU.java

--- src/ur_os/virtualmemory/PVMM_LRU.java
+++ src/ur_os/virtualmemory/PVMM_LRU.java
@@ -20,8 +20,30 @@ public class PVMM_LRU extends ProcessVirtualMemoryManager{
     public int getVictim(LinkedList<Integer> memoryAccesses, int loaded) {
         
         //ToDo
+        LinkedList<Integer> recent = new LinkedList<>();
+        int size  = memoryAccesses.size() -1 ;
+
+
+        while(size >= 0 && recent.size() < loaded){
+
+            int access = memoryAccesses.get(size);
+
+            if(!recent.contains(access)){
+                //add to recent accesses if not in list
+                recent.addFirst(access);
+
+            }else{
+                //move to front if accessed again
+                int indx = recent.indexOf(access);
+                recent.remove(indx);
+                recent.addFirst(access);
+            }
+
+            size--;
+        }
         
-        return -1;
+        //return the least recently used page
+        return recent.getLast();
     }
     
 }
