f0caff720bb55581fd61c51901df0c47de08e9db
QueenR1014 <juanreinareyes@hotmail.com>
2025-11-09 23:52:19 -0500

    MFU

--- src/ur_os/virtualmemory/PVMM_MFU.java
+++ src/ur_os/virtualmemory/PVMM_MFU.java
@@ -4,6 +4,7 @@
  */
 package ur_os.virtualmemory;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 /**
@@ -19,9 +20,37 @@ public class PVMM_MFU extends ProcessVirtualMemoryManager{
     @Override
     public int getVictim(LinkedList<Integer> memoryAccesses, int loaded) {
         
-        //ToDo
-        
-        return -1;
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        LinkedList<Integer> recent = new LinkedList<>();
+        int size = memoryAccesses.size();
+
+
+        //Create Frequency Map
+        for(int i = size -1; i >= 0 ; i--){
+            int page = memoryAccesses.get(i); 
+            freq.put(page, freq.getOrDefault(page, 0) + 1);//add frecuencies
+            
+            // recently loaded pages (most recent first)
+            if(recent.size() < loaded && !recent.contains(page)){
+                recent.add(page);
+            }
+        }
+         
+
+        // Victim Selection
+        int victim = -1;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < loaded; i++){
+            int page = recent.get(i);
+            int pageFreq = freq.get(page);
+            if(pageFreq > max){
+                victim = page;
+                max = pageFreq;
+            }
+        }
+
+
+        return victim;
     }
     
 }
